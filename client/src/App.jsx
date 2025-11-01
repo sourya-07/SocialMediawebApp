eecafbe52889930162dafa1bc6fc33135feb2020
Sourya <souryagupta2005@gmail.com>
2025-11-01 18:40:49 +0530

    Routes path for all the pages done and login page completed also used clerk for login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Messages from './pages/Messages'
+import ChatBox from './pages/ChatBox'
+import Connections from './pages/Connections'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
+import Feed from './pages/Feed'
+import {useUser} from '@clerk/clerk-react'
 
 const App = () => {
+  const {user} = useUser()
   return (
-    <div>
-
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={ !user ? <Login /> : <Layout /> }>
+          <Route index element={<Feed />} />
+          <Route path='messages' element={<Messages />} />
+          <Route path='messages/:userId' element={<ChatBox />} />
+          <Route path='connections' element={<Connections />} />
+          <Route path='discover' element={<Discover />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='profile/:profileId' element={<Profile />} />
+          <Route path='create-post' element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
