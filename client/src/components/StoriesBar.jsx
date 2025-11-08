bd512c0f0d1f057ff08a653802865eb9bfdaab88
Sourya <souryagupta2005@gmail.com>
2025-11-08 17:09:06 +0530

    Create Photo/Video upload button, Create Story Button and added X button to cloe the Story

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -3,6 +3,7 @@ import { dummyStoriesData } from '../assets/assets'
 import { Plus } from 'lucide-react'
 import moment from 'moment'
 import StoryModel from './StoryModal'
+import StoryViewer from './StoryViewer'
 
 const StoriesBar = () => {
 
@@ -35,7 +36,7 @@ const StoriesBar = () => {
                 {/* Story Cards */}
                 {
                     stories.map((story, index) => (
-                        <div key={index} className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`} >
+                        <div onClick={() => setViewStory(story)} key={index} className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`} >
                             <img src={story.user.profile_picture} alt='' className='absolute size-8 top-3 left-3 z-10 rounded-full ring ring-gray-100 shadow' />
                             <p className='absolute top-18 left-3 text-white/60 text-sm truncate max-w-24'>{story.content}</p>
                             <p className='text-white absolute bottom-1 right-2 z-10 text-xs'>{moment(story.createdAt).fromNow()}</p>
@@ -60,6 +61,8 @@ const StoriesBar = () => {
             {
                 showModal && <StoryModel setShowModal={setShowModal} fetchStories={fetchStories}/>
             }
+            {/* View Story Modal */}
+            {viewStory && <StoryViewer  viewStory={viewStory} setViewStory={setViewStory}/>}
 
         </div>
     )
--- client/src/components/StoryModal.jsx
+++ client/src/components/StoryModal.jsx
@@ -1,9 +1,10 @@
-import { ArrowLeft } from 'lucide-react'
+import { ArrowLeft, Sparkle, TextIcon, Upload } from 'lucide-react'
 import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 
 const StoryModal = ({ setShowModal, fetchStories }) => {
 
-    const bgColors = ['#4f46e5', '#7c3aed', '#db2777', '#e11d48', '#ca8a04', '#0d9488']
+    const bgColors = ['#4f46e5', '#7c3aed', '#db2777', '#e11d48', '#ca8a04', '#0d9488', "#000000", '#ffffff']
 
     const [mode, setMode] = useState('text')
     const [background, setBackground] = useState(bgColors[0])
@@ -16,12 +17,12 @@ const StoryModal = ({ setShowModal, fetchStories }) => {
         if (file) {
             setMedia(file)
             setPreviewUrl(URL.createObjectURL(file))
-
+            // setMode('media')
         }
     }
 
     const handleCreateStory = async () => {
-
+        
     }
 
     return (
@@ -52,9 +53,29 @@ const StoryModal = ({ setShowModal, fetchStories }) => {
 
                 <div className='flex mt-4 gap-2'>
                     {bgColors.map((color) => (
-                        <button key={color} className='w-6 h-6 rounded-full ring cursor-pointer' style={{backgroundColor: color}} onClick={() => setBackground(color)}/>
+                        <button key={color} className='w-6 h-6 rounded-full ring cursor-pointer' style={{ backgroundColor: color }} onClick={() => setBackground(color)} />
                     ))}
                 </div>
+                
+                <div className='flex gap-2 mt-4'>
+                    <button onClick={() => {setMode('text'); setMedia(null);
+                        setPreviewUrl(null)
+                    }} className={`flex-1 flex items-center justify-center gap-2 p-2 rounded cursor-pointer ${mode === 'text' ? 'bg-white text-black' : "bg-zinc-800"}`}>
+                        <TextIcon size={18}/> Text
+                    </button>
+                    <label className={`flex-1 flex items-center justify-center gap-2 p-2 rounded cursor-pointer ${mode === 'media' ? "bg-white text-black" : "bg-zinc-800"}`}>
+                        <input onChange={(e) => {handelMediaUpload(e); setMode('media')}} type="file" accept='image/*, video/*' className='hidden'/>
+                        <Upload size={18}/> Photo/Video
+                    </label>
+                </div>
+                
+                <button onClick={() =>toast.promise(handleCreateStory(), {
+                    loading: 'Saving...',
+                    success: <p>Story Added</p>,
+                    error: e => <p>{e.message}</p>
+                })} className='flex items-center justify-center gap-2 text-white py-3 mt-4 w-full rounded bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition cursor-pointer'>
+                    <Sparkle size={18}/> Create Story
+                </button>
 
             </div>
 
