cac9a9d00b256199b5bc0ee2d1c233a3365c20c6
Sourya <souryagupta2005@gmail.com>
2025-11-10 07:02:50 +0530

    Created the user model

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './configs/db.js'
 
 const app = express()
 
+await connectDB()
+
 app.use(express.json())
 app.use(cors())
-
 
 app.get('/', (req, res) => {
     res.send("Server is running")
