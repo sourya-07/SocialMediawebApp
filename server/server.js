96d12f75ef598192454a9855733003366c481993
Sourya <souryagupta2005@gmail.com>
2025-11-10 09:13:45 +0530

    Created crud operation using inngest

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/db.js'
+import {inngest, functions} from './inngest/index.js'
 
 const app = express()
 
@@ -14,7 +15,13 @@ app.get('/', (req, res) => {
     res.send("Server is running")
 })
 
+app.use('/api/inngest', serve({ client: inngest, functions }))
+
 const PORT = process.env.port || 4000
+
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
