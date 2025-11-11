40379d4fd05c5a4cbfca1a627df985630ec3c448
Sourya <souryagupta2005@gmail.com>
2025-11-11 06:32:15 +0530

    Completed the authentication

---
+++ server/middlewares/auth.js
@@ -0,0 +1,19 @@
+// import { err } from "inngest/types"
+
+export const protect = async (req, res, next) => {
+    try {
+        const { userId } = req.auth()
+        if (!userId) {
+            return res.json({
+                success: false,
+                message: "not authenticated"
+            })
+        }
+        next()
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ import 'dotenv/config'
 import connectDB from './configs/db.js'
 import {inngest, functions} from './inngest/index.js'
 import {serve} from 'inngest/express'
+import { clerkMiddleware } from '@clerk/express'
+import userRouter from './routes/userRotes.js'
+
 
 
 const app = express()
@@ -12,13 +15,15 @@ connectDB()
 
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 
 app.get('/', (req, res) => {
     res.send("Server is running")
 })
 app.use('/api/inngest', serve({ client: inngest, functions }))
-
+app.use('/api/inngest', serve({client: inngest, functions}))
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 4000
 
