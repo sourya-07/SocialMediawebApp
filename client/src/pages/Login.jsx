20939fc1b0a3c51d9223be257a82626e17ec2596
Sourya <souryagupta2005@gmail.com>
2025-11-16 09:34:24 +0530

    hh

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
             </div>
           </div>
           <h1 className='text-3xl md:text-6xl md:pb-2 font-bold bg-gradient-to-r from-indigo-950 to-indigo-800 bg-clip-text text-transparent'>More than just friends truly connect</h1>
-          <p className='text-xl md:text-3xl text-indigo-900 max-w-72 md:max-w-md'>connect with global community on pingup.</p>
+          <p className='text-xl md:text-3xl text-indigo-900 max-w-72 md:max-w-md'>connect with global community on UNIGRAM.</p>
         </div>
         <span className='md:h-10'></span>
       </div>
